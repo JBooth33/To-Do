98e19c035f85f6fbf06438cd65411cb9aa5e1995
Joe Booth <joseph.booth3@gmail.com>
2018-06-15 07:32:27 -0500

    adding get and post routes to tasks_controller

--- controllers/tasks_controller.js
+++ controllers/tasks_controller.js
@@ -2,9 +2,27 @@ var express = require("express");
 
 var router = express.Router();
 // Import the model (task.js) to use its database functions.
-var cat = require("../models/task.js");
+var task = require("../models/task.js");
 
 //create router and export at end of file
+router.get("/", function(req, res) {
+    task.all(function(data) {
+        var hbsObject = {
+            taks: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
+router.post("api/taks", function(req, res) {
+    task.create([
+        "task_name", "completed"
+    ], [
+        req.body.task_name, req.body.completed
+    ], function(result) {
+        res.json({ id: result.insertID });
+    });
+});
 
 module.exports = router;
