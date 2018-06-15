e2ccc40ba1b3fe007d89b5d38cf8c184c64fabbe
Joe Booth <joseph.booth3@gmail.com>
2018-06-15 10:01:53 -0500

    adding index.handlebars

--- models/task.js
+++ models/task.js
@@ -2,6 +2,24 @@
 var orm = require("../config/orm.js");
 
 //create the code that will call the ORM functions using task specific input for the ORM.
+var task = {
+    all: function(cb) {
+        orm.all("tasks", function(res) {
+            cb(res);
+        });
+    },
 
+    create: function(cols, vals, cb) {
+        orm.create("tasks", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(objColVals, condition, cb) {
+        orm.update("cats", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
 // Export the database functions for the controller (catsController.js).
-//module.exports = task;
+module.exports = task;
